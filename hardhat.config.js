61274f485943b501d0bcdf2d52d4a771c8881c38
Naresh Rao <nareshrao@brugu.io>
2023-03-12 17:14:35 +0530

    remove test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,3 @@
-// require("@nomicfoundation/hardhat-toolbox");
-
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.17",
-// };
-
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
